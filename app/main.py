e18eefb2fc42668e73d702c1fc68786bded8a3c9
Julien BOLLINI <julien.bollini@gmail.com>
2025-12-09 16:11:26 +0100

    Load to JSON

--- app/main.py
+++ app/main.py
@@ -1,7 +1,9 @@
 from fastapi import FastAPI
+import pandas as pd
 import os
 
 app = FastAPI()
+
 
 @app.get("/")
 def read_root():
@@ -12,3 +14,35 @@ def read_root():
 @app.get("/health")
 def health_check():
     return {"status": "ok"}
+
+@app.get("/get-process-data")
+def get_process_data():
+    try:
+        # Si votre structure est app/data/data.csv et WORKDIR est /code
+        csv_path = "app/data/data.csv"
+
+        # Vérification si le fichier existe
+        if not os.path.exists(csv_path):
+            return {"error": f"Fichier non trouvé au chemin : {csv_path}"}
+
+        # Lecture du CSV
+        df = pd.read_csv(csv_path)
+
+        # FILTRAGE : On ne garde que les colonnes utiles
+        colonnes_a_garder = ['sample', 'xmeas_7', 'xmeas_9', 'xmeas_10']
+
+        # On sélectionne uniquement ces colonnes
+        # Si une colonne manque, cela lèvera une erreur (ce qui est mieux que d'envoyer de mauvaises données)
+        df_filtered = df[colonnes_a_garder]
+
+        # 3. Conversion en JSON
+        return df_filtered.to_dict(orient='records')
+
+        # On convertit le DataFrame en dictionnaire (JSON compatible)
+        # orient='records' crée une liste d'objets : [{"Pression": 10, ...}, ...]
+        data_json = df.to_dict(orient='records')
+
+        return data_json
+
+    except Exception as e:
+        return {"error": str(e)}
